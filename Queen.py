858152a6b60d4cf0c8b2b3c79d0043d65fe1f2f3
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-05-08 09:07:51 -0400

    Added Movement Queen.py

--- Queen.py
+++ Queen.py
@@ -6,9 +6,21 @@ class Queen(Piece):
     def __init__(self, x, y, team, character, canvas, cell_size):
         super().__init__(x, y, team, character, canvas, cell_size)
 
-        # If piece pos can move to new cell x,y then move
-        # Only check if valid move
-
     def canMoveTo(self, from_x, from_y, to_x, to_y, chess_board):
-        return True
+        print(f"From: {from_x}, {from_y} To: {to_x}, {to_y}")
+        isValidMove = False
 
+        if chess_board[to_y][to_x] is None:
+            # ----- Side ways movement -----
+            if from_x is not to_x and from_y == to_y:
+                isValidMove = True
+
+            # Up and Down movement
+            if from_y is not to_y and from_x == to_x:
+                isValidMove = True
+
+            # ----- Diagnol Movement -----
+            if abs(to_x - from_x) == abs(to_y - from_y):
+                isValidMove = True
+
+        return isValidMove
