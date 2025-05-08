d34dee31e896359f18daadb4b95dee1415cf27bd
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-05-08 09:07:36 -0400

    Added Movement King.py

--- King.py
+++ King.py
@@ -5,11 +5,16 @@ import BuildBoard
 class King(Piece):
     def __init__(self, x, y, team, character, canvas, cell_size):
         super().__init__(x, y, team, character, canvas, cell_size)
-
-        # If piece pos can move to new cell x,y then move
-        # Only check if valid move
+        self.has_moved = False
 
     def canMoveTo(self, from_x, from_y, to_x, to_y, chess_board):
-        return True
+        print(f"From: {from_x}, {from_y} To: {to_x}, {to_y}")
+        isValidMove = False
 
+        if chess_board[to_y][to_x] is None:
+            x_diff = abs(to_x - from_x)
+            y_diff = abs(to_y - from_y)
+            if x_diff <= 1 and y_diff <= 1:
+                isValidMove = True
 
+        return isValidMove
